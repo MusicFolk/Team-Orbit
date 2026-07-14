fa115c42b02848316369187ec73efe614e567332
Matiss <matissciss@gmail.com>
2026-07-14 11:35:42 +0300

    Fixed constructor issue in UserDetailServiceImpl

--- src/main/java/com/orbit/team/security/UserDetailServiceImplementation.java
+++ src/main/java/com/orbit/team/security/UserDetailServiceImplementation.java
@@ -2,9 +2,7 @@ package com.orbit.team.security;
 
 import com.orbit.team.entity.User;
 import com.orbit.team.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,12 +10,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class UserDetailServiceImplementation implements UserDetailsService {
 
     private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(()-> new UsernameNotFoundException("There is no user with this username: " + username));
         return new UserSecurity(user);
