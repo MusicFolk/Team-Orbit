cfff9dd792ada940d1646ef72752e07a94b9fbe0
Vytautas Povilauskas <povilauskasvytautas@gmail.com>
2026-07-14 13:18:17 +0300

    RegisterUser blank spaces validation

--- src/main/java/com/orbit/team/dto/request/RegisterRequest.java
+++ src/main/java/com/orbit/team/dto/request/RegisterRequest.java
@@ -2,12 +2,14 @@ package com.orbit.team.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class RegisterRequest {
     @NotBlank
+    @Pattern(regexp = "^\\S+$", message = "Username must contain no blank spaces")
     private String username;
     @NotBlank
     private String fullName;
