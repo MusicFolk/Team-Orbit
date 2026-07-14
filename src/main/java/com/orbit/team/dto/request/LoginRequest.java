e0b1e771f40c48dfdbeae58f14b5663a8389ebb0
Vytautas Povilauskas <povilauskasvytautas@gmail.com>
2026-07-14 12:25:15 +0300

    LoginRequest, RegisterRequest update

--- src/main/java/com/orbit/team/dto/request/LoginRequest.java
+++ src/main/java/com/orbit/team/dto/request/LoginRequest.java
@@ -7,8 +7,8 @@ import lombok.Data;
 @Data
 public class LoginRequest {
     @NotBlank
-    @Email
-    private String email;
+    private String username;
+
     @NotBlank
     private String password;
 }
--- src/main/java/com/orbit/team/dto/request/RegisterRequest.java
+++ src/main/java/com/orbit/team/dto/request/RegisterRequest.java
@@ -7,6 +7,8 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
     @NotBlank
+    private String username;
+    @NotBlank
     private String fullName;
     @NotBlank
     @Email
