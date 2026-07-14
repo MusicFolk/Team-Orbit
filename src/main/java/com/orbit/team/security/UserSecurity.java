73140623f392cb755391f00824f51744bd103914
Matiss <matissciss@gmail.com>
2026-07-14 14:51:53 +0300

    Added getUser() method in UserSecurity

--- src/main/java/com/orbit/team/security/UserSecurity.java
+++ src/main/java/com/orbit/team/security/UserSecurity.java
@@ -17,6 +17,10 @@ public class UserSecurity implements UserDetails {
         this.user = user;
     }
 
+    public User getUser() {
+        return user;
+    }
+
     public Long getId() {
         return user.getId();
     }
