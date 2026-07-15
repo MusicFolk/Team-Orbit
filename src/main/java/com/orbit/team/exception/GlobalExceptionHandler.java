b432229fc5860e7b3f6be80336756773d43216ce
martHaza <martazapacka17@gmail.com>
2026-07-15 16:32:16 +0300

    added @ResponseBody

--- src/main/java/com/orbit/team/exception/GlobalExceptionHandler.java
+++ src/main/java/com/orbit/team/exception/GlobalExceptionHandler.java
@@ -4,22 +4,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseBody
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ex.getMessage());
     }
 
+    @ResponseBody
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<String> handleDuplicateEmail(DuplicateEmailException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ex.getMessage());
     }
 
+    @ResponseBody
     @ExceptionHandler(UnauthorizedActionException.class)
     public ResponseEntity<String> handleUnauthorizedAction(UnauthorizedActionException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
