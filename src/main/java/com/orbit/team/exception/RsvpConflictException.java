d001cb2cc62900d6db1e9ebbe7d84932ce46658a
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-07-16 12:46:36 +0300

    Fix formatting in RsvpConflictException

--- src/main/java/com/orbit/team/exception/RsvpConflictException.java
+++ src/main/java/com/orbit/team/exception/RsvpConflictException.java
@@ -1,7 +1,11 @@
 package com.orbit.team.exception;
 
 public class RsvpConflictException extends RuntimeException {
-  public RsvpConflictException(String message) {
-    super(message);
-  }
+    public RsvpConflictException(String message) {
+        super(message);
+    }
+
+    public RsvpConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
